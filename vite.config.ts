00d61e173d722ad1591338f51f8b2c8eff81fc7a
antonio-pas <antonio.b.passos18@gmail.com>
2023-01-14 22:05:22 -0600

    fixed vite browser problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    target: ["esnext"]
+  },
   resolve: {
     alias: {
       "$assets": "/src/assets",
